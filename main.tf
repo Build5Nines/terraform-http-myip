c4a1f98d0cc5a886d5865b6b18f242caa8a5eb72
Chris Pietschmann <chris@build5nines.com>
2026-03-06 18:45:07 -0500

    add about info

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+# #######################################################
+# Build5Nines MyIP Terraform Module
+# Retrieve Local IP for PC running Terraform
+#
+# Source:
+# https://github.com/Build5Nines/terraform-http-myip
+#
+# Author: Chris Pietschmann
+# Copyright (c) 2026 Build5Nine LLC (https://build5nines.com)
+# #######################################################
+
 variable "url" {
   description = "The URL to query for the public IP address. Defaults to https://ipv4.icanhazip.com (hosted by Cloudflare), which returns the caller's IPv4 address in plain text."
   default     = "https://ipv4.icanhazip.com"
